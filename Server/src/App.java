abe9986b4737ebc91c5a30dff665eae4e672a2de
royalecode <edmonbosch@gmail.com>
2021-04-06 20:39:10 +0200

    interfice confDAO done

--- Server/src/App.java
+++ Server/src/App.java
@@ -1,8 +1,9 @@
+import Persistance.ConfDAO;
 import Persistance.ConfigurationDAO;
 
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
-        //ConfigurationDAO confDAO = new ConfigurationDAO("Server/src/Data/configuracio-servidor.json");
+        //ConfDAO confDAO = new ConfigurationDAO("Server/src/Data/configuracio-servidor.json");
     }
 }
