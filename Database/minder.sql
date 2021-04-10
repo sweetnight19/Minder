fda9eb70cb52df695ab2a344105add234f5e9c35
David Marquet <david.marquet@students.salle.url.edu>
2021-04-10 13:14:12 +0200

    hotfix on bbdd relationships

--- Database/minder.sql
+++ Database/minder.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 19-03-2021 a las 20:58:48
+-- Tiempo de generación: 10-04-2021 a las 13:05:19
 -- Versión del servidor: 10.4.17-MariaDB
 -- Versión de PHP: 8.0.2
 
@@ -34,7 +34,9 @@ CREATE TABLE IF NOT EXISTS `pair` (
   `idOrigen` int(11) NOT NULL,
   `idDesti` int(11) NOT NULL,
   `matchDuo` tinyint(1) NOT NULL,
-  `data` date NOT NULL
+  `data` date NOT NULL,
+  KEY `pair_ibfk_2` (`idDesti`),
+  KEY `pair_ibfk_1` (`idOrigen`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -56,7 +58,27 @@ CREATE TABLE IF NOT EXISTS `usuari` (
   `descripcio` text NOT NULL,
   `llenguatgeDeProgramacio` varchar(255) NOT NULL,
   PRIMARY KEY (`uuid`,`nickname`,`email`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `usuari`
+--
+
+INSERT INTO `usuari` (`nomPila`, `nickname`, `edat`, `uuid`, `tipusCompte`, `email`, `password`, `pathImage`, `descripcio`, `llenguatgeDeProgramacio`) VALUES
+('Edmon Bosch', 'boschedmon', 21, 1, 'Normal', 'edmonbosch@students.salle.url.edu', 'hola', '/pathincompleto', 'Sóc l\'Edmon estudio GTAS i m\'encanta la programació, de gran vull treballar a Google o Facebook.', 'Java'),
+('David Marquet', 'david.marquet', 25, 2, 'Premium', 'david.marquet@students.salle.url.edu', 'hola2', '/pathincomplete', 'Hola més endavant ja editare la meva descripció', 'Java'),
+('Joan Casals', 'joan.casals', 21, 3, 'Premium', 'joan.casals@students.salle.url.edu', 'hola3', '/pathincomplete', 'Hola més endavant ja editare la meva descripció', 'Javascript'),
+('Xavier Garrigos', 'xavier.garrigos', 21, 4, 'Normal', 'xavier.garrigos@students.salle.url.edu', 'hola4', '/pathicnomplete', 'Hola més endavant ja editare la meva descripció', 'CSS'),
+('Pol Muñoz', 'pol.munoz', 23, 5, 'Premium', 'pol.munoz@salle.url.edu', 'hola5', '/pathincomplete', 'Hola més endavant ja editare la meva descripció', 'Java'),
+('Artur Alcoverro', 'artur.alcoverro', 21, 6, 'Normal', 'artur.alcoverro@students.salle.url.edu', 'hola6', '/pathincomplete', 'Hola més endavant ja editare la meva descripció', 'Javascript'),
+('Xavier Gomez', 'xavier.gomez', 26, 7, 'Normal', 'xavier.gomez@students.salle.url.edu', 'hola7', '/pathincomplete', 'Hola més endavant ja editare la meva descripció', 'html'),
+('Ricard Viñolas', 'richard.vinolas', 21, 8, 'Normal', 'ricard.vinolas@students.salle.url.edu', 'hola8', '/pathincomplete', 'Hola més endavant ja editare la meva descripció', 'html'),
+('Elon Musk', 'elon.musk', 38, 9, 'Premium', 'elon.musk@salle.url.edu', 'hola10', '/pathincomplete', 'Hola més endavant ja editare la meva descripció', 'C'),
+('Bill Gates', 'bill.gates', 58, 10, 'Normal', 'bill.gates@salle.url.edu', 'hola11', '/pathincomplete', 'Hola més endavant ja editare la meva descripció', 'Javascript'),
+('Emily Ermion', 'ermion.potter', 24, 11, 'Normal', 'emily.ermion@salle.url.edu', 'hola12', '/pathincomplete', 'Hola més endavant ja editare la meva descripció', 'C'),
+('Jennifer Aniston', 'jenifer.aniston', 45, 12, 'Normal', 'jenifer.aniston@salle.url.edu', 'hola13', '/pathincomplete', 'Hola més endavant ja editare la meva descripció', 'css'),
+('Angela Merkel', 'angela.merkel', 52, 13, 'Normal', 'angela.merkel@salle.url.edu', 'hola14', '/pathincomplete', 'Hola més endavant ja editare la meva descripció', 'html'),
+('Carla Divinity', 'diva.carla', 27, 14, 'Normal', 'carla.divinity@salle.url.edu', 'hola15', '/pathincomplete', 'Hola més endavant ja editare la meva descripció', 'Javascript');
 
 -- --------------------------------------------------------
 
@@ -69,8 +91,28 @@ CREATE TABLE IF NOT EXISTS `xat` (
   `idOrigen` int(11) NOT NULL,
   `idDesti` int(11) NOT NULL,
   `missatge` text NOT NULL,
-  `data` date NOT NULL
+  `data` date NOT NULL,
+  KEY `xat_ibfk_1` (`idOrigen`),
+  KEY `xat_ibfk_2` (`idDesti`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `pair`
+--
+ALTER TABLE `pair`
+  ADD CONSTRAINT `pair_ibfk_1` FOREIGN KEY (`idOrigen`) REFERENCES `usuari` (`uuid`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `pair_ibfk_2` FOREIGN KEY (`idDesti`) REFERENCES `usuari` (`uuid`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `xat`
+--
+ALTER TABLE `xat`
+  ADD CONSTRAINT `xat_ibfk_1` FOREIGN KEY (`idOrigen`) REFERENCES `usuari` (`uuid`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `xat_ibfk_2` FOREIGN KEY (`idDesti`) REFERENCES `usuari` (`uuid`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
