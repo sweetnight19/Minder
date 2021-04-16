8139015680016c8e29d1d1f4490753516431ec50
Xavi <xavier.garrigos@students.salle.url.edu>
2021-04-16 21:48:55 +0200

    test-ui-definitiu

--- Client/src/App.java
+++ Client/src/App.java
@@ -1,9 +1,12 @@
 import Persistance.ConfigurationDAO;
 import Persistance.JsonConfigurationDAO;
+import Presentation.views.MinderView;
 
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        MinderView view = new MinderView();
+        view.display();
         //ConfigurationDAO confDAO = new JsonConfigurationDAO("Client/Data/configuracio-client.json");
     }
 }
